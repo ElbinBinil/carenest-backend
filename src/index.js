42da626802c8f2209a9ebf32d2e8e5b5fdb7f380
Elbin Binil <elbinbinil@gmail.com>
2025-01-17 12:10:43 +0530

    Init setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+// import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 3001, () => {
+//       console.log(`Server is running on the port ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log("Connection error ..", error);
+//   });
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
