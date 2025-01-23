b57d616185ae3926ca9b3eb91e0492ef8bafb652
Elbin Binil <elbinbinil@gmail.com>
2025-01-23 09:08:45 +0530

    db setup conifg

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
-// import connectDB from "./db/index.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-// connectDB()
-//   .then(() => {
-//     app.listen(process.env.PORT || 3001, () => {
-//       console.log(`Server is running on the port ${process.env.PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log("Connection error ..", error);
-//   });
-app.listen(process.env.PORT || 3001, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3001, () => {
+      console.log(`Server is running on the port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Connection error ..", error);
+  });
